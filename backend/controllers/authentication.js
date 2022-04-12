bbf62cde87745c4016a677391834d174fa537b77
RamaTanikella <88115546+RamaTanikella@users.noreply.github.com>
2022-04-11 23:24:35 -0400

    iam4

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
@@ -14,9 +15,24 @@ router.post('/', async (req, res) => {
             message: 'could not find a user with the provided username and password'
         })
     } else {
-        res.json({user})
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({user: user, token: result.value })
     }
     console.log('IN HERE')
+})
+
+___
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId:  
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
 })
 
 module.exports = router
